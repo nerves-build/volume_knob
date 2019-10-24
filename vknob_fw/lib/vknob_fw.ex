72d11ee21f4731954628696ba54cdaec2081d2b7
Steven Fuchs <stwf@mac.com>
2019-10-24 08:08:16 -0400

    nerves install

---
+++ vknob_fw/lib/vknob_fw.ex
@@ -0,0 +1,18 @@
+defmodule VknobFw do
+  @moduledoc """
+  Documentation for VknobFw.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VknobFw.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ vknob_fw/test/vknob_fw_test.exs
@@ -0,0 +1,8 @@
+defmodule VknobFwTest do
+  use ExUnit.Case
+  doctest VknobFw
+
+  test "greets the world" do
+    assert VknobFw.hello() == :world
+  end
+end
