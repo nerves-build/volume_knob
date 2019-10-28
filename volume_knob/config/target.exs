2cee175bb6d9e39f97f22adde42e343dd589b80f
stwf <stwf@yahoo.com>
2019-10-28 16:54:46 -0400

    lights work

--- volume_knob/config/target.exs
+++ volume_knob/config/target.exs
@@ -3,7 +3,8 @@ use Mix.Config
 config :rotary_encoder, RotaryEncoder.Monitor,
   switch_gpio: 23,
   encoder_a_gpio: 24,
-  encoder_b_gpio: 25
+  encoder_b_gpio: 25,
+  led_base_address: 0x68
 
 config :sonex, Sonex.Discovery,
   net_device_name: "wlan0"
