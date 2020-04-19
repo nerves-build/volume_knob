3fcf231e809e205b30eda930835cb9193cd81356
Steven Fuchs <stwf@mac.com>
2020-04-18 23:52:22 -0400

    Update host.exs

--- volume_knob/config/host.exs
+++ volume_knob/config/host.exs
@@ -4,7 +4,7 @@ config :rotary_encoder, RotaryEncoder, gpio_handler: RotaryEncoder.MockGpioPin
 
 config :tlc59116, Tlc59116, i2c_handler: Tlc59116.MockI2CPin
 
-config :sonex, Sonex.Discovery, net_device_name: "en0j"
+config :sonex, Sonex.Discovery, net_device_name: "en0"
 
 config :volume_knob, VolumeKnob.VolumeState,
   state_location: "/Users/steve/root/vol_knob_data.term"
